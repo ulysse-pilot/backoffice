468e2826e41b73380d9467e82e31e6f73fdcf4e7
Gaston Pascal tonguino <pgastong03@gmail.com>
2024-10-19 20:00:55 +0000

    update table view

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-// import { ColumnDef } from "@tanstack/react-table";
 import React from "react";
-// import {
-//   flexRender,
-//   getCoreRowModel,
-//   useReactTable,
-// } from "@tanstack/react-table";
 
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 type ParkingType = {
   id: string;
@@ -48,8 +42,6 @@ export default function Home() {
   //   columns,
   //   getCoreRowModel: getCoreRowModel(),
   // });
-
-  
 
   React.useEffect(() => {
     fetchParkings();
@@ -102,79 +94,60 @@ export default function Home() {
               </div>
             </div>
           ) : parkings && parkings.length > 0 ? (
-            parkings.map((parking) => (
-              <div
-                key={parking.id}
-                className="flex items-center justify-between space-x-4"
-              >
-                {/* <Image
-                  src={"https://via.placeholder.com/150"}
-                  alt={parking.fullName}
-                  width={70}
-                  height={70}
-                  className="w-12 h-12 rounded-full"
-                /> */}
-                <div className="flex flex-col">
-                  <p className="text-sm text-slate-700 font-medium">
-                    {parking.fullName}
-                  </p>
-                  <p className="text-xs text-slate-500 font-medium">
-                    {parking.phoneNumber}
-                  </p>
-                </div>
-                <Button variant={"secondary"} size={"sm"}>
-                  View details
-                </Button>
-              </div>
-            ))
-            // <Table>
-            //   <TableHeader>
-            //     {table.getHeaderGroups().map((headerGroup) => (
-            //       <TableRow key={headerGroup.id}>
-            //         {headerGroup.headers.map((header) => {
-            //           return (
-            //             <TableHead key={header.id}>
-            //               {header.isPlaceholder
-            //                 ? null
-            //                 : flexRender(
-            //                     header.column.columnDef.header,
-            //                     header.getContext()
-            //                   )}
-            //             </TableHead>
-            //           );
-            //         })}
-            //       </TableRow>
-            //     ))}
-            //   </TableHeader>
-            //   <TableBody>
-            //     {table.getRowModel().rows?.length ? (
-            //       table.getRowModel().rows.map((row) => (
-            //         <TableRow
-            //           key={row.id}
-            //           data-state={row.getIsSelected() && "selected"}
-            //         >
-            //           {row.getVisibleCells().map((cell) => (
-            //             <TableCell key={cell.id}>
-            //               {flexRender(
-            //                 cell.column.columnDef.cell,
-            //                 cell.getContext()
-            //               )}
-            //             </TableCell>
-            //           ))}
-            //         </TableRow>
-            //       ))
-            //     ) : (
-            //       <TableRow>
-            //         <TableCell
-            //           colSpan={columns.length}
-            //           className="h-24 text-center"
-            //         >
-            //           No results.
-            //         </TableCell>
-            //       </TableRow>
-            //     )}
-            //   </TableBody>
-            // </Table>
+            // parkings.map((parking) => (
+            //   <div
+            //     key={parking.id}
+            //     className="flex items-center justify-between space-x-4"
+            //   >
+            //     {/* <Image
+            //       src={"https://via.placeholder.com/150"}
+            //       alt={parking.fullName}
+            //       width={70}
+            //       height={70}
+            //       className="w-12 h-12 rounded-full"
+            //     /> */}
+            //     <div className="flex flex-col">
+            //       <p className="text-sm text-slate-700 font-medium">
+            //         {parking.fullName}
+            //       </p>
+            //       <p className="text-xs text-slate-500 font-medium">
+            //         {parking.phoneNumber}
+            //       </p>
+            //     </div>
+            //     <Button variant={"secondary"} size={"sm"}>
+            //       View details
+            //     </Button>
+            //   </div>
+            // ))
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Id</TableHead>
+                  <TableHead>Full name</TableHead>
+                  <TableHead>Phone number</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {parkings.length ? (
+                  parkings.map((parking) => (
+                    <TableRow
+                      key={parking.id}
+                      // data-state={row.getIsSelected() && "selected"}
+                    >
+                      <TableCell className="w-[100px]">{parking.id}</TableCell>
+                      <TableCell>{parking.fullName}</TableCell>
+                      <TableCell>{parking.phoneNumber}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={4} className="h-24 text-center">
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
           ) : (
             <div className="flex items-center justify-center h-full">
               <div className="spinner-border text-primary" role="status">
